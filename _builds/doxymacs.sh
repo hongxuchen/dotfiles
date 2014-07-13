d165d417298e405736a0c008c712c6aaeed0af52
HongxuChen <leftcopy.chx@gmail.com>
2014-07-13 13:17:03 +0800

    debian testing

--- _builds/doxymacs.sh
+++ _builds/doxymacs.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
 BASEDIR="`dirname $0`"
-SRC=$BASEDIR/repos/doxymacs-1.8.0
+cd $BASEDIR
+REPOS=$(pwd)/repos
+SRC=${REPOS}/doxymacs-1.8.0
 
 if ! [ -d $SRC ]; then
+    cd ${REPOS}
     wget ${SRC} http://downloads.sourceforge.net/project/doxymacs/doxymacs/1.8.0/doxymacs-1.8.0.tar.gz 
     tar xvf ${SRC}.tar.gz
     rm ${SRC}.tar.gz
 fi
 
-cd $SRC
-./configure --prefix=$HOME/.emacs.d/site-lisp/doxymacs --with-default-style=JavaDoc
+cd ${SRC}
+
+./configure --prefix=$HOME/.emacs.d/site-lisp/doxymacs #--with-default-style=JavaDoc
 make
 make install
