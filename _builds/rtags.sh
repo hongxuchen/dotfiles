c3e1c42824bb39124593dc93712ea4d67f08497a
HongxuChen <leftcopy.chx@gmail.com>
2015-03-22 18:07:25 +0800

    update rtags.sh

--- _builds/rtags.sh
+++ _builds/rtags.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-BASEDIR=$(dirname $0)
-cd $BASEDIR
+BASEDIR=$(dirname "$0")
+cd "$BASEDIR"
 SRC=$(pwd)/rtags
 BUILD=$SRC/build
 ELISP=$HOME/.emacs.d/config/rtags
@@ -11,28 +11,32 @@ rm -f ~/.bin/clang++
 rm -f ~/.bin/gcc
 rm -f ~/.bin/g++
 
-if ! [ -d $SRC ]; then
-    git clone --recursive git@github.com:Andersbakken/rtags.git  $SRC
+if ! [ -d "$SRC" ]; then
+    git clone --recursive git@github.com:Andersbakken/rtags.git  "$SRC"
 else
-    cd $SRC
+    cd "$SRC"
     git pull --all
     git submodule update --recursive
 fi
 
-rm $ELISP
+rm "$ELISP"
 git checkout master
 
-rm -rf $BUILD && mkdir -p $BUILD && cd $BUILD
-C_INCLUDE_PATH=
-CPLUS_INCLUDE_PATH=
-LD_LIBRARY_PATH=
+rm -rf "$BUILD" && mkdir -p "$BUILD" && cd "$BUILD"
+export C_INCLUDE_PATH=
+export CPLUS_INCLUDE_PATH=
+export LD_LIBRARY_PATH=
 export PATH=/usr/bin:$PATH
-cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -GNinja $SRC  
-ninja
+# cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -GNinja "$SRC"
+# ninja
+# sudo ninja install
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 "$SRC"
+make
+sudo make install
 
-ln -sf $SRC/bin/gcc-rtags-wrapper.sh ~/.bin/g++
-ln -sf $SRC/bin/gcc-rtags-wrapper.sh ~/.bin/gcc
-ln -sf $SRC/bin/gcc-rtags-wrapper.sh ~/.bin/clang++
-ln -sf $SRC/bin/gcc-rtags-wrapper.sh ~/.bin/clang
+ln -sf "$SRC"/bin/gcc-rtags-wrapper.sh ~/.bin/g++
+ln -sf "$SRC"/bin/gcc-rtags-wrapper.sh ~/.bin/gcc
+ln -sf "$SRC"/bin/gcc-rtags-wrapper.sh ~/.bin/clang++
+ln -sf "$SRC"/bin/gcc-rtags-wrapper.sh ~/.bin/clang
 
-ln -sf $SRC $ELISP
+ln -sf "$SRC" "$ELISP"
