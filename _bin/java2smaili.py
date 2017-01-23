314a6de404dbf0b82776a183c5a3887f54ee7e6a
Hongxu <hongxuchen1989@gmail.com>
2017-01-23 18:09:00 +0800

    linux for android

---
+++ _bin/java2smaili.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import os
+import shutil
+import subprocess
+
+version = "1.6"
+
+name = "_java2smaili"
+
+dex_type = "apk"
+
+
+def require(cond, msg):
+    if not cond:
+        raise Exception(msg)
+
+
+def run(cmd):
+    print("=> {}".format(cmd))
+    subprocess.call(cmd.split())
+
+
+def java2jar(d):
+    java_files = [f for f in os.listdir(d) if f.endswith(".java")]
+    for f in java_files:
+        cmd = "javac -source {} -target {} {}".format(version, version, f)
+        run(cmd)
+    class_files = [f for f in os.listdir(d) if f.endswith(".class")]
+    jar_file = name + ".jar"
+    cmd = "jar cvf {} {}".format(jar_file, " ".join(class_files))
+    run(cmd)
+    require(os.path.exists(jar_file), "{} not generated".format(jar_file))
+    for f in class_files:
+        os.remove(f)
+    return jar_file
+
+
+def jar2smali(jar_file):
+    dex_file = os.path.abspath(name + "." + dex_type)
+    cmd = "dx --dex --output={} {}".format(dex_file, jar_file)
+    run(cmd)
+    require(os.path.exists(dex_file), "{} not generated".format(dex_file))
+    os.remove(jar_file)
+    cmd = "apktool -f d {}".format(dex_file)
+    run(cmd)
+    os.remove(dex_file)
+
+
+def tear_up():
+    shutil.rmtree(name, ignore_errors=True)
+
+
+def java2smali(d):
+    tear_up()
+    jar_file = java2jar(d)
+    jar2smali(jar_file)
+
+
+if __name__ == '__main__':
+    java2smali(os.curdir)
--- _sbt/0.13/plugins/android.sbt
+++ _sbt/0.13/plugins/android.sbt
@@ -1,1 +1,1 @@
-// addSbtPlugin("org.scala-android" % "sbt-android" % "1.7.0")
+addSbtPlugin("org.scala-android" % "sbt-android" % "1.7.2")
