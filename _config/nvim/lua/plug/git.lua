b41e771da1c9be22ac0d6c712e34b4f8b68d5c0f
hongxuchen <hongxu_chen@foxmail.com>
2024-09-16 22:56:12 +0800

    feat: remove Git-Messager vim plugin

--- _config/nvim/lua/plug/git.lua
+++ _config/nvim/lua/plug/git.lua
@@ -1,12 +1,5 @@
 return {
-  {
-    "rhysd/git-messenger.vim",
-    cmd = "GitMessenger",
-    config = function()
-      vim.g.git_messenger_no_default_mappings = true
-    end,
-  },
-  -- fugitive does not need to setup
+  -- no need to setup
   { "tpope/vim-fugitive" },
-  { "junegunn/gv.vim"},
+  { "junegunn/gv.vim" },
 }
