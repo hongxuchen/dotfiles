589a552285945169f415f5640a086e634a58fbed
hongxuchen <hongxu_chen@foxmail.com>
2024-10-20 16:16:33 +0800

    feat(nvim): neotest configurations

--- _config/nvim/lua/plug/unittest.lua
+++ _config/nvim/lua/plug/unittest.lua
@@ -1,19 +1,22 @@
--- TODO: https://www.reddit.com/r/neovim/comments/108nc7g/introducing_continuoustestingnvim/
--- NOTE: neotest-rust is NOT needed since we have LSP codelens
 return {
   "nvim-neotest/neotest",
-  -- keys = {"<localleader>uu", "<localleader>ud", "<localleader>uf"},
+  keys = {"<localleader>uu", "<localleader>ud", "<localleader>uf"},
   lazy = true,
   dependencies = {
     "nvim-neotest/neotest-plenary",
     "nvim-neotest/neotest-python",
+    "alfaix/neotest-gtest",
+    "mrcjkb/rustaceanvim",
   },
   config = function()
     local t = require("neotest")
+    ---@diagnostic disable-next-line: missing-fields
     t.setup {
       adapters = {
         require("neotest-python"),
         require("neotest-plenary"),
+        require("rustaceanvim.neotest"),
+        require("neotest-gtest").setup {},
       },
     }
     local u = require("core.utils")
@@ -21,14 +24,15 @@ return {
       t.run.attach()
     end, u.opts, "[neotest] attach test")
     u.keymap("n", "<localleader>ud", function()
+      ---@diagnostic disable-next-line: missing-fields
       t.run.run { strategy = "dap" }
     end, u.opts, "[neotest] nearest debug test")
     u.keymap("n", "<localleader>uf", function()
       local fname = vim.api.nvim_buf_get_name(0)
       t.run.run(fname)
-    u.keymap("n", "<localleader>uu", function()
-      t.run.run()
-    end, u.opts, "[neotest] nearest test")
+      u.keymap("n", "<localleader>uu", function()
+        t.run.run()
+      end, u.opts, "[neotest] nearest test")
     end, u.opts, "[neotest] file test")
     u.keymap("n", "<localleader>us", function()
       t.run.stop()
