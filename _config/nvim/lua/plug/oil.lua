eae1a207620cb20790a5eb3293a23b744523868b
hongxuchen <hongxu_chen@foxmail.com>
2025-01-27 14:33:22 +0800

    feat(nvim): oil configurations

--- _config/nvim/lua/plug/oil.lua
+++ _config/nvim/lua/plug/oil.lua
@@ -5,13 +5,31 @@ return {
   opts = {
     default_file_explorer = true,
     watch_for_changes = true,
+    skip_confirm_for_simple_edits = true,
+    view_options = {
+      show_hidden = true,
+      case_insensitive = true,
+      sort = {
+        { "name", "asc" },
+        { "type", "asc" },
+      },
+    },
+    git = {
+      -- Return true to automatically git add/mv/rm files
+      add = function(path)
+        return true
+      end,
+      mv = function(src_path, dest_path)
+        return true
+      end,
+      rm = function(path)
+        return false
+      end,
+    },
   },
-  -- config = function()
-  --   require("oil").setup {}
-  -- end,
   keys = {
     {
-      "<leader>e",
+      "-",
       "<Cmd>Oil<CR>",
       desc = "[oil] open file explorer",
     },
